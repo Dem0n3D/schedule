9dff656ed38042615b9d53bbd9378e7ca4d4e277
Дмитрий Баранов <baranov@semograph.com>
2017-05-15 12:30:36 +0500

    Добавлена модель корпуса.

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .classroom import Classroom
 from .group import Group
 from .lesson import Lesson
 from .teacher import Teacher
+from .corpus import Corpus
--- models/corpus.py
+++ models/corpus.py
@@ -4,11 +4,8 @@ from sqlalchemy.orm import relationship, backref
 from alchemy import db
 
 
-class Classroom(db.Model):
+class Corpus(db.Model):
 
     id = Column(Integer, primary_key=True)
 
     number = Column(String(255), nullable=False)
-
-    corpus_id = Column(Integer, ForeignKey("corpus.id"), nullable=False)
-    corpus = relationship("Corpus", backref="classrooms")
